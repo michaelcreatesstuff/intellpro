3617f1fe59f510dfbcf9cd571211e6d3fbebb78e
Michael Yang <michaelcreatesstuff@gmail.com>
2022-04-11 12:46:37 -0700

    change base url to use env var

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,17 +1,18 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
-export const createOrUpdateLotString = async (id, payload) => await api.post(`/lotString/${id}`, payload);
+export const createOrUpdateLotString = async (id, payload) =>
+  await api.post(`/lotString/${id}`, payload);
 export const getLotsStringsIds = async () => await api.get(`/lotStringsIds`);
 export const getLotStringById = async (id) => await api.get(`/lotString/${id}`);
 
 const apis = {
   createOrUpdateLotString,
   getLotsStringsIds,
-  getLotStringById
+  getLotStringById,
 };
 
 export default apis;
